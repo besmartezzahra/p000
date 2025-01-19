7278b9b02021d42165c01547ca6713a5a834096d
besmartezzahra <127043088+besmartezzahra@users.noreply.github.com>
2025-01-19 10:42:24 +0100

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,5 +1,11 @@
-def main():
-    print("Hello, World!")
+import subprocess
+
+def run_javascript_file(file_path):
+    try:
+        result = subprocess.run(['node', file_path], capture_output=True, text=True)
+        print(result.stdout)
+    except Exception as e:
+        print(f"An error occurred: {e}")
 
 if __name__ == "__main__":
-    main()
+    run_javascript_file('src/script.js')
